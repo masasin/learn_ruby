acdd3ddd9b6dc07905f6fd0a71de5c31ed0ee737
jnassar <jn.masasin@gmail.com>
2023-08-19 02:40:39 +0200

    Basic types

---
+++ giraffe_academy/00_basic_types.rb
@@ -0,0 +1,13 @@
+# Printing
+puts "Hello"  # With a new line
+print "World"  # Without a new line
+puts "!"
+
+# String interpolation
+name = "Jean"
+puts "Hello #{name}, how are you?"
+
+# Casting
+puts "A" + 1.to_s
+puts 3.14.to_i
+puts "3.0".to_f
--- giraffe_academy/00_printing.rb
+++
@@ -1,3 +0,0 @@
-puts("Hello")
-print("World")
-puts("!")
