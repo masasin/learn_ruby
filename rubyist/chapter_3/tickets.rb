0034fce5adb05b2c0d898fa158f3eac7e51ec6fa
jnassar <jn.masasin@gmail.com>
2023-09-06 23:11:22 +0200

    Do up to 3.6.3.

--- rubyist/chapter_3/tickets.rb
+++ rubyist/chapter_3/tickets.rb
@@ -14,6 +14,10 @@ class Ticket
   def price
     @full_price * (100 - (@discount or 0)) / 100.0
   end
+
+  def self.most_expensive(*tickets)
+    tickets.max_by(&:price)
+  end
 end
 
 th = Ticket.new("Town Hall", "2013-11-12")
@@ -21,3 +25,11 @@ th.price = 100
 puts "The ticket costs $#{format('%.2f', th.price)}."
 th.discount = 15
 puts "With a #{format('%.2f', th.discount)}% discount, it will cost $#{format('%.2f', th.price)}."
+
+cc = Ticket.new("Convention Center", "2014-12-13")
+cc.price = 90
+
+lt = Ticket.new("Lincoln Theater", "2015-11-12")
+lt.price = 75
+
+puts "The highest-priced ticket is the one for #{Ticket.most_expensive(th, cc, lt).venue}."
