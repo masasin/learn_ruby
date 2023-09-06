3ebd8a289157930f4ab3da049892e5075365f816
jnassar <jn.masasin@gmail.com>
2023-09-06 22:27:38 +0200

    Make discount visible.

--- rubyist/chapter_3/tickets.rb
+++ rubyist/chapter_3/tickets.rb
@@ -1,10 +1,10 @@
 class Ticket
   attr_reader :venue, :date
-  attr_accessor :full_price
+  attr_accessor :full_price, :discount
 
   def initialize(venue)
     @venue = venue
-    @_discount = 0
+    @discount = 0
   end
 
   def date=(date)
@@ -21,11 +21,7 @@ class Ticket
   end
 
   def price
-    @full_price * (100 - @_discount) / 100.0
-  end
-
-  def discount=(percent)
-    @_discount = percent
+    @full_price * (100 - @discount) / 100.0
   end
 end
 
@@ -34,4 +30,4 @@ th.date = "2013-11-12"
 th.price = 100
 puts "The ticket costs $#{format('%.2f', th.price)}."
 th.discount = 15
-puts "With a 15% discount, it will cost $#{format('%.2f', th.price)}."
+puts "With a #{format('%.2f', th.discount)}% discount, it will cost $#{format('%.2f', th.price)}."
