0151371450d0e071d18391913b8d21bb066c75ac
jnassar <jn.masasin@gmail.com>
2023-09-06 22:23:18 +0200

    Avoid modifying when setting the discount.

--- rubyist/chapter_3/tickets.rb
+++ rubyist/chapter_3/tickets.rb
@@ -1,39 +1,34 @@
 class Ticket
+  attr_reader :venue, :date
+
   def initialize(venue)
     @venue = venue
-  end
-
-  def venue
-    @venue
+    @_discount = 0
   end
 
   def date=(date)
     split_date = date.split("-")
-    if split_date.length != 3 or split_date[0].length != 4 or split_date[1].length != 2 or split_date[2].length != 2
+    if split_date.length != 3 || split_date[0].length != 4 || split_date[1].length != 2 || split_date[2].length != 2
       puts "Please submit the date in the format 'YYYY-MM-DD'."
     else
       @date = date
     end
   end
 
-  def date
-    @date
-  end
-
   def price=(amount)
     if (amount * 100).to_i == amount * 100
-      @price = amount.to_f
+      @_original_price = amount.to_f
     else
       puts "The price seems to be malformed."
     end
   end
 
   def price
-    @price
+    @_original_price * (100 - @_discount) / 100.0
   end
 
-  def discount!(percent)
-    @price = @price * (100 - percent) / 100.0
+  def discount=(percent)
+    @_discount = percent
   end
 end
 
@@ -41,4 +36,5 @@ th = Ticket.new("Town Hall")
 th.date = "2013-11-12"
 th.price = 100
 puts "The ticket costs $#{format('%.2f', th.price)}."
-puts "With a 15% discount, it will cost $#{format('%.2f', th.discount!(15))}."
+th.discount = 15
+puts "With a 15% discount, it will cost $#{format('%.2f', th.price)}."
